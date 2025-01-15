fb8d4e3812180fc221b0fcb2e4a582d996f26a0c
Dave Young <dyoung@coreweave.com>
2025-01-15 16:26:57 -0600

    chore: use the proper comparison operator

--- pkg/exporters/k8sexporter/problemclient/problem_client.go
+++ pkg/exporters/k8sexporter/problemclient/problem_client.go
@@ -144,7 +144,7 @@ func (c *nodeProblemClient) DeleteDeprecatedConditions(ctx context.Context, cond
 
 	// update the node status if we need to,
 	// we only need to if the number of conditions has changed
-	if len(newConditions) == len(node.Status.Conditions) {
+	if len(newConditions) < len(node.Status.Conditions) {
 		// node.Status.Conditions = newConditions
 		// _, err = c.client.Nodes().UpdateStatus(ctx, node, metav1.UpdateOptions{})
 		// if err != nil {
